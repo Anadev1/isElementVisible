f44582a4eb21001d997f5a920c2d1d8ce983aeb1
Ana <eaaania@students.eaaa.dk>
2020-02-12 10:32:19 +0100

    coding video

--- js/audio.js
+++ js/audio.js
@@ -2,26 +2,42 @@
  * fil: audio.js
  * purpose: play audio when the containing div is visible
  **/
-window.addEventListener("scroll", function() { // detect scroll event
+window.addEventListener("scroll", function () { // detect scroll event
 
-  var mySound = document.getElementById("myaudio"); // select audio id
-  var lyd = document.getElementById("lyd");
-  console.log("Er div #lyd synlig? " + elFllVsbl(lyd)); // test
+    var mySound = document.getElementById("myaudio"); // select audio id
+    var myVideo = document.getElementById("myvideo");
+    var lyd = document.getElementById("lyd");
+    var vid = document.getElementById("video")
+    console.log("Er div #lyd synlig? " + elFllVsbl(lyd)); // test
+    
+    myVideo.muted = true;
 
-// Below: start and stop audio 
-  if (elFllVsbl(lyd)) { // if the #lyd div is visible then
-    if (!(mySound.curentTime > 0)) {
-      mySound.play(); // play audio
+    // Below: start and stop audio 
+    if (elFllVsbl(lyd)) { // if the #lyd div is visible then
+        if (!(mySound.curentTime > 0)) {
+            mySound.play(); // play audio
+        }
+        
+    } else {
+        mySound.pause();
+        mySound.currentTime = 0; // rewind the sound
     }
-  } else {
-    mySound.pause();
-    mySound.currentTime = 0; // rewind the sound
-  }
+    
+        if (elFllVsbl(vid)) { // if the #lyd div is visible then
+        if (!(myVideo.curentTime > 0)) {
+            myVideo.play(); // play audio
+        }
+        
+    } else {
+        myVideo.pause();
+        myVideo.currentTime = 0; // rewind the sound
+    }
+    
 })
 
 // this function will check whether a tag is visible
 function elFllVsbl(el) {
-  return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
+    return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
 }
 
 /*
